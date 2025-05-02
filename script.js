07bbad5cce51a2a06133fc3e94f33cd3dbc82b96
testjoelops <testjoelops@outlook.com>
2025-05-02 18:43:04 +0530

    Update script.js
    form id

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('crm_lead-form').addEventListener('submit', function(eve
     event.preventDefault();
     
     // Hide the form
-    document.getElementById('lcrm_lead-form').classList.add('hidden');
+    document.getElementById('crm_lead-form').classList.add('hidden');
     
     // Show the thank you message
     document.getElementById('thank-you-message').classList.remove('hidden');
